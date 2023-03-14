cd9d555667dc9b58fcf4f360bbb3e95659b3fdc0
Jhonatan Soto <jhonrocker2012@gmail.com>
2023-03-13 22:59:33 -0400

    add icons input dark mode

--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -1,10 +1,24 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.scss'],
 })
-export class MainNavComponent {
+export class MainNavComponent implements OnInit {
+  @ViewChild('checkboxDarkMode') public checkBoxDarkMode!: ElementRef<HTMLInputElement>;
 
+  ngOnInit(): void {
+
+  }
+
+  public changeTheme(): void {
+    // if (this.checkBoxDarkMode.nativeElement.checked) {
+    document.body.classList.toggle('dark-theme');
+    // } else {
+    //
+    // }
+    // console.log(this.checkBoxDarkMode.nativeElement.checked, 'checkout');
+    // document.body.classList.toggle('dark-theme');
+  }
 }
