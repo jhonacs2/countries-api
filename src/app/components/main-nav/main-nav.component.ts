94d6a47d08f1870c7a0f31907cd08a3854cae129
Jhonatan Soto <jhonrocker2012@gmail.com>
2023-03-21 20:31:29 -0400

    save theme to local storage

--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ThemeService } from '../../shared/services/theme.service';
+import { filter, first } from 'rxjs';
+import { ThemeMode } from '../../interfaces/ThemeMode.enum';
 
 @Component({
   selector: 'app-main-nav',
@@ -9,17 +11,21 @@ import { ThemeService } from '../../shared/services/theme.service';
 export class MainNavComponent implements OnInit {
   @ViewChild('checkboxDarkMode') public checkBoxDarkMode!: ElementRef<HTMLInputElement>;
 
+  public darkTheme: boolean;
 
   constructor( private _themeService: ThemeService ) {
+    this.darkTheme = false;
   }
 
   ngOnInit(): void {
-
+    this._themeService.themeChanged$
+      .pipe(first(), filter(value => value === ThemeMode.DARK))
+      .subscribe(() => {
+        this.darkTheme = true;
+      });
   }
 
   public changeTheme(): void {
     this._themeService.updateTheme();
-    // document.body.classList.toggle('light');
-    // document.body.classList.toggle('dark');
   }
 }
--- src/app/shared/services/theme.service.ts
+++ src/app/shared/services/theme.service.ts
@@ -19,6 +19,9 @@ export class ThemeService {
   }
 
   private _init(): void {
+    const themeLocalStorage = this._getThemeLocalStore();
+    this._currentMode = themeLocalStorage as ThemeMode;
+    this._themeChangedSubject.next(themeLocalStorage as ThemeMode);
     this._document.body.classList.add(this._currentMode);
   }
 
@@ -27,8 +30,20 @@ export class ThemeService {
     this._document.body.classList.toggle(ThemeMode.DARK);
     if (this._currentMode === ThemeMode.LIGHT) {
       this._currentMode = ThemeMode.DARK;
+      this._updateLocalStore(this._currentMode);
+      this._themeChangedSubject.next(this._currentMode);
     } else {
       this._currentMode = ThemeMode.LIGHT;
+      this._themeChangedSubject.next(this._currentMode);
+      this._updateLocalStore(this._currentMode);
     }
+  }
+
+  private _updateLocalStore( theme: string ): void {
+    localStorage.setItem('ca-theme', this._currentMode);
+  }
+
+  private _getThemeLocalStore(): string {
+    return localStorage.getItem('ca-theme') || ThemeMode.LIGHT;
   }
 }
